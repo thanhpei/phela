accfbb94fa28046f7a57636778833ac8cee95b72
Bamboo-rat <linhxinh414@gmail.com>
2025-12-02 02:03:43 +0700

    done

--- fe_phela/vite.config.ts
+++ fe_phela/vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: "./",
+  base: "/",
   build: {
     outDir: "build/client",
     emptyOutDir: true,
